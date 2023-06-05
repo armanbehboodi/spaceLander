704c3aaa1a9644de14b77d81b85181df9ef386a4
Arman <arman.behboodi99@gmail.com>
2023-06-05 14:06:44 +0330

    fix bugs

--- assets/js/script.ts
+++ assets/js/script.ts
@@ -8,8 +8,11 @@ $(function () {
         deviceWidth: number = $body.width(),
         deviceHeight: number = $body.height(),
         height: number = Number($rocket.css('top').replace('px', '')),
-        angle: number = 10,
-        numberOfStars: number = 1;
+        deviation: number = 10,
+        velocity: number = 0,
+        angle: number = 0,
+        numberOfStars: number = 1,
+        missionDone: boolean = false;
 
     //background handler (blinking stars)
     while (numberOfStars < 155) {
@@ -33,9 +36,9 @@ $(function () {
     //falling simulator function
     const fallingSimulator = (): void => {
         if (Number($rocket.css('bottom').replace('px', '')) > 0) {
-            $rocket.css({top: `${height}px`, transform: `translateY(17px) rotate(${angle}deg)`});
+            $rocket.css({top: `${height += velocity}px`, transform: `rotate(${deviation += angle}deg)`});
         } else {
-            if (Math.abs(angle) > 2) {
+            if (Math.abs(deviation) > 5 || velocity > 1) {
                 let explosionTimer: number = 0,
                     explosionInterval: number = setInterval(() => {
                         if (explosionTimer < 50) {
@@ -48,64 +51,51 @@ $(function () {
                     }, 15);
                 $explosionSoundEffect[0].play();
             }
+            missionDone = true;
             clearInterval(fallingInterval);
         }
     }
 
     //falling simulation timer
     let fallingInterval: number = setInterval((): void => {
-        height += 1;
-        angle += 0.1;
+        velocity += 0.005;
+        if (deviation > 0 && deviation < 180) {
+            angle += 0.001;
+        } else {
+            angle -= 0.001;
+        }
         fallingSimulator();
     }, 25);
 
-
-    //rocket controlling function
-    let controllingInterval,
-        counter: number = 0.5;
-
     //activating rocket controllers
     $body.on('keydown', function (e): void {
-        let key = e.key,
-            bottom: number = Math.abs(Number($rocket.css('bottom').replace('px', '')));
+        let key = e.key;
 
-        if (key.charCodeAt(0) == 65 && key !== 'ArrowDown' && bottom > 1) {
+        if (key.charCodeAt(0) == 65 && key !== 'ArrowDown' && !missionDone) {
             $engineSoundEffect[0].play();
             switch (key) {
                 case 'ArrowUp':
                     $mainFlame.css({height: `${Math.random() * 11 + 15}px`});
-                    clearInterval(controllingInterval);
-                    $rocket.css({transition: 'all 0.5s'});
-                    height -= 5;
+                    if (Math.abs(deviation) > 90) {
+                        velocity += 0.025;
+                    } else {
+                        velocity -= 0.025;
+                    }
                     break;
                 case 'ArrowRight':
                     $('#sl-flame-right').css({width: `${Math.random() * 6 + 10}px`});
-                    angle -= 0.5;
+                    angle -= 0.01;
                     break;
                 case 'ArrowLeft':
                     $('#sl-flame-left').css({width: `${Math.random() * 6 + 10}px`});
-                    angle += 0.5;
+                    angle += 0.01;
                     break;
             }
         }
     })
-        .on('keyup', function (e): void {
-            let isBoosterOn: boolean = $rocket.css('transition').indexOf('0.5') !== -1;
+        .on('keyup', function (): void {
             $mainFlame.css({height: '0'});
             $sideFlames.css({width: '0'});
-
-            if (e.key == 'ArrowUp' && isBoosterOn) {
-                controllingInterval = setInterval((): void => {
-                    if (counter > 0.025) {
-                        counter -= 0.025;
-                        $rocket.css({transition: `all ${counter}s`});
-                    } else {
-                        counter = 0.5;
-                        clearInterval(controllingInterval);
-                    }
-                }, 250);
-            }
-
             $engineSoundEffect[0].pause();
         })
 })
